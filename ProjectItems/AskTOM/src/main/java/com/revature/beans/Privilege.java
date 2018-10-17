0b604a7c4446c7330e7213c3ac20ddc71b35c3bf
erikmoore <erikmoore001@icloud.com>
2018-10-17 16:07:52 -0400

    beans: tom, priv - mapped to cfg.xml

--- ProjectItems/AskTOM/src/main/java/com/revature/beans/Privilege.java
+++ ProjectItems/AskTOM/src/main/java/com/revature/beans/Privilege.java
@@ -1,9 +1,26 @@
 package com.revature.beans;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="priv")
 public class Privilege {
 
+		@Id
+		@Column(name="priv_id")
+		@SequenceGenerator(sequenceName="priv_seq", name="priv_seq")
+		@GeneratedValue(generator="priv_seq", strategy=GenerationType.SEQUENCE)
 		Integer id;
+		
+		@Column(name="priv_name")
 		String name;
+		
 		public Integer getId() {
 			return id;
 		}
