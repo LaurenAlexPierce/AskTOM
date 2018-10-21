337d00f9f4626aa0d787ef9650ce6a3f30fd0a7f
gregebriggs <gregebriggs@gmail.com>
2018-10-21 12:38:40 -0700

    Setup getTom service.

--- ProjectItems/AngularTom/tom-angular/Angular/src/app/services/tom.service.ts
+++ ProjectItems/AngularTom/tom-angular/Angular/src/app/services/tom.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders} from '@angular/common/http';
+import { Tom } from '../components/tom/tom';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TomService {
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+
+  httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json'})
+  }
+
+  getTom(){
+    return this.http.get<Tom>("http://");
+  }
+
 }
