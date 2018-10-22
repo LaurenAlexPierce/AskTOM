e8f42e1f03cfbe39066cfe1ecc98e4e56297e661
gregebriggs <gregebriggs@gmail.com>
2018-10-22 15:12:52 -0700

    Refactor style and login page.

--- ProjectItems/AngularTom/tom-angular/Angular/src/app/services/tom.service.ts
+++ ProjectItems/AngularTom/tom-angular/Angular/src/app/services/tom.service.ts
@@ -15,21 +15,23 @@ export class TomService {
   }
 
   getTopics(){
-    return this.http.get<Topic[]>("http://18.191.49.49:8080/AskTOM/GetTopics");
+    return this.http.get<Topic[]>("http://localhost:8085/AskTOM/GetTopics");
   }
   
   getTom(username:string, password:string){
     let body = new HttpParams();
+    /* Point to tomcat server */
     let headers = new HttpHeaders().set(
       'Content-Type', 'application/x-www-form-urlencoded'
     );
 
       body = body.set('username', username);
       body = body.set('password', password);
-
-    return this.http.post<Tom>("http://18.191.49.49:8080/AskTOM/LoginServlet",
+      /* Point to tomcat server */
+    return this.http.post<Tom>("http://loaclhost:8085/AskTOM/LoginServlet",
     body,
     {headers:headers});
   }
 
+  
 }
