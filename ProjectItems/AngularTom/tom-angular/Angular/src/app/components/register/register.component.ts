f353ae92a8b20870f461841109374a2e011e5ccc
gregebriggs <gregebriggs@gmail.com>
2018-10-24 11:40:54 -0700

    fix navigation between login and register.

--- ProjectItems/AngularTom/tom-angular/Angular/src/app/components/register/register.component.ts
+++ ProjectItems/AngularTom/tom-angular/Angular/src/app/components/register/register.component.ts
@@ -1,3 +1,4 @@
+import { Tom } from './../tom/tom';
 import { NavbarComponent } from './../navbar/navbar.component';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
@@ -7,10 +8,18 @@ import { Router } from '@angular/router';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
+  
+  newTom:Tom = new Tom("","");
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  addTom(){
+    new Tom(
+      this.newTom.username,
+      this.newTom.password
+    )
+  }
 }
