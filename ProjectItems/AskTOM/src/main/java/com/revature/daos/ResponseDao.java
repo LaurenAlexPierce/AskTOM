238c3bb278c0e8d410f5e6bcfb82b31f56f6dd1d
erikmoore <erikmoore001@icloud.com>
2018-10-24 14:30:21 -0400

    added new method to response dao

--- ProjectItems/AskTOM/src/main/java/com/revature/daos/ResponseDao.java
+++ ProjectItems/AskTOM/src/main/java/com/revature/daos/ResponseDao.java
@@ -10,4 +10,5 @@ public interface ResponseDao {
 	public Response selectResponseById(Integer id);
 	public Response updateResponseById(Response r);
 	public void removeResponse(Integer id);
+	public List<Response> getAllResponseByTopic(Integer id);
 }
