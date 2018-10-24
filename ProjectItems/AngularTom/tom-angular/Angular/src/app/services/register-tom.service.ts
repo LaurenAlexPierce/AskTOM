efac623869d95d5bac565408d05e9b7c8f7996af
gregebriggs <gregebriggs@gmail.com>
2018-10-24 15:18:46 -0700

    Add body to register-tom service.Revert Tom service for login http post.

--- ProjectItems/AngularTom/tom-angular/Angular/src/app/services/register-tom.service.ts
+++ ProjectItems/AngularTom/tom-angular/Angular/src/app/services/register-tom.service.ts
@@ -1,9 +1,34 @@
 import { Injectable } from '@angular/core';
+import { HttpParams, HttpHeaders, HttpClient } from '@angular/common/http';
+import { Tom } from '../components/tom/tom';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RegisterTomService {
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+
+  httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json'})
+  }
+
+  addTom(username:string, password:string, passwordConfirm:string, privId:number){
+    let body = new HttpParams();
+    /* Point to tomcat server */
+    let headers = new HttpHeaders().set(
+      'Content-Type', 'application/x-www-form-urlencoded'
+    );
+
+      body = body.set('username', username);
+      body = body.set('password', password);
+      body = body.set('passwordConfirm', passwordConfirm)
+      body = body.set('privId', privId.toString());
+      /* Point to tomcat server */
+    return this.http.post<Tom>("http://loaclhost:8085/AskTOM/RegisterServlet",
+    body,
+    {headers:headers});
+  }
+
+  
 }
