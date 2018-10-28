3c9c6b849faebe6a7c8151e45aaecc77f89b7c92
Lauren Pierce <lauren.pierce141@yahoo.com>
2018-10-27 19:33:45 -0500

    Using HtmlUnitTestDriver

--- ProjectItems/AskTOM/src/test/java/com/revature/tests/RegisterTest.java
+++ ProjectItems/AskTOM/src/test/java/com/revature/tests/RegisterTest.java
@@ -1,34 +1,39 @@
-//package com.revature.tests;
-//
-//import org.openqa.selenium.By;
-//import org.openqa.selenium.WebDriver;
-//import org.openqa.selenium.firefox.FirefoxDriver;
-//import org.testng.annotations.AfterClass;
-//import org.testng.annotations.BeforeClass;
-//import org.testng.annotations.Test;
-//
-//public class RegisterTest {
-//  public static WebDriver driver;
-//  public final String url = "http://18.219.234.217:8085/AskTom/register";
-//	
-//  @BeforeClass
-//  public void setup() {
-//	  System.setProperty("webdriver.gecko.driver", "drivers/geckodriver");
-//	  driver = new FirefoxDriver();
-//	  driver.get(url);
-//  }
-//
-//  @AfterClass
-//  public void teardown() throws InterruptedException{
-//	  driver.quit();
-//  }
-//  
-//  
-//  @Test
-//  public void registerTom() {
-//	  driver.findElement(By.name("register-username")).sendKeys("newUser");
-//	  driver.findElement(By.name("register-password")).sendKeys("newPassword");
-//	  driver.findElement(By.name("register-password-confirm")).sendKeys("newPassword");
-//	  driver.findElement(By.name(""));
-//  }
-//}
+package com.revature.tests;
+
+import java.util.ArrayList;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import com.gargoylesoftware.htmlunit.BrowserVersion;
+
+public class RegisterTest {
+  public static HtmlUnitDriver driver;
+  public String url;
+  ArrayList<WebElement> elements;
+  
+  @BeforeClass
+  public void setup() {
+	  driver = new HtmlUnitDriver(BrowserVersion.CHROME);
+	  url = "http://18.219.234.217:8085/AskTom/register";
+	  driver.get(url);
+  }
+
+  @AfterClass
+  public void teardown() throws InterruptedException{
+	  driver.quit();
+  }
+  
+  
+  @Test
+  public void registerTom() {
+	  driver.findElement(By.name("register-username")).sendKeys("newUser");
+	  driver.findElement(By.name("register-password")).sendKeys("newPassword");
+	  driver.findElement(By.name("register-password-confirm")).sendKeys("newPassword");
+	  driver.findElement(By.name(""));
+  }
+}
